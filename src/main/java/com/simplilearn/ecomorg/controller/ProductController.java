6453917f59c1554cf32d91874fbe9a688bd3a5f1
Alzohar Tech <alzohartech@gmail.com>
2023-08-26 23:36:10 +0530

    Added spring crud operation with product

--- src/main/java/com/simplilearn/ecomorg/controller/ProductController.java
+++ src/main/java/com/simplilearn/ecomorg/controller/ProductController.java
@@ -1,10 +1,14 @@
 package com.simplilearn.ecomorg.controller;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,10 +25,47 @@ public class ProductController {
 		return products;
 	}
 	
+	@GetMapping("/products/{productId}")
+	public Product getProduct(@PathVariable int productId){
+		
+		for (Product product : products) {
+			if(product.getProductId() == productId)
+				return product;
+		}
+		return null;
+	}
+	
 	@PostMapping("/products")
 	public List<Product> addProduct(@RequestBody Product product){
-		int id = product.
+		products.add(product);
 		return products;
 	}
 	
+	@PutMapping("/products")
+	public List<Product> updateProduct(@RequestBody Product product){
+		if (product != null) { 
+			for (int index = 0; index < products.size(); index++) { 
+				if (products.get(index).getProductId() == product.getProductId() ) {
+					// replace /update on products list object
+					products.set(index, product);
+					return products;
+				}
+			}
+		}
+		return null;
+	}
+	
+	@DeleteMapping("/products/{productId}")
+	public List<Product> updateProduct(@PathVariable int productId){
+		if (productId>0) { 
+			for (int index = 0; index < products.size(); index++) { 
+				if (products.get(index).getProductId() == productId) {
+					// remove match  products list object
+					products.remove(index);
+					return products;
+				}
+			}
+		}
+		return null;
+	}
 }
