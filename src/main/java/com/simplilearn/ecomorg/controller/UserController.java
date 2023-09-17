20d375df6336fdc55c8ce35dbb610c62c406db35
Alzohar Tech <alzohartech@gmail.com>
2023-09-17 22:28:40 +0530

    cors config with update in user mng

--- src/main/java/com/simplilearn/ecomorg/controller/UserController.java
+++ src/main/java/com/simplilearn/ecomorg/controller/UserController.java
@@ -1,15 +1,20 @@
 package com.simplilearn.ecomorg.controller;
 
+import java.util.Date;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.simplilearn.ecomorg.dto.ResponseDto;
 import com.simplilearn.ecomorg.entity.User;
 import com.simplilearn.ecomorg.service.UserService;
 
@@ -20,29 +25,54 @@ public class UserController {
 	@Autowired 
 	UserService userService;
 	
+	/**
+	 * Get all users
+	 * @return
+	 */
 	@GetMapping("/users")
-	public List<User> getUsers(){
-		return userService.getUsers();
+	public Page<User> getUsers(@RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "10") int size, 
+			@RequestParam(defaultValue = "userId") String sort, @RequestParam(defaultValue = "ASC") String sortOrder){
+		return userService.getUsers(page,size, sort, sortOrder);
 	}
 	
+	/**
+	 * Get one user
+	 * @param userId
+	 * @return
+	 */
 	@GetMapping("/users/{userId}")
 	public User getUser(@PathVariable int userId){
 		return userService.getUser(userId);
 	}
 	
+	/**
+	 * Add user
+	 * @param user
+	 * @return
+	 */
 	@PostMapping("/users")
 	public User addUser(@RequestBody User user){
 		return userService.addUser(user);
 	}
 	
+	/**
+	 * Update user
+	 * @param user
+	 * @return
+	 */
 	@PutMapping("/users")
 	public User updateUser(@RequestBody User user){
 		return userService.updateUser(user);
 	}
 	
+	/**
+	 * Delete user
+	 * @param userId
+	 * @return
+	 */
 	@DeleteMapping("/users/{userId}")
-	public String updateUser(@PathVariable int userId){
+	public ResponseDto updateUser(@PathVariable int userId){
 		userService.deleteUser(userId);
-		return "User is deleted sucessfully with userId : "+userId;
+		return new ResponseDto("User is deleted sucessfully with userId : "+userId, new Date(),HttpStatus.OK.name(),null);
 	}
 }
--- src/main/java/com/simplilearn/ecomorg/service/UserService.java
+++ src/main/java/com/simplilearn/ecomorg/service/UserService.java
@@ -3,12 +3,17 @@ package com.simplilearn.ecomorg.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.simplilearn.ecomorg.dto.LoginRequestDto;
 import com.simplilearn.ecomorg.entity.Admin;
 import com.simplilearn.ecomorg.entity.User;
+import com.simplilearn.ecomorg.exception.BadRequestException;
 import com.simplilearn.ecomorg.exception.NotFoundException;
 import com.simplilearn.ecomorg.repository.UserRepository;
 
@@ -21,8 +26,11 @@ public class UserService {
 	BCryptPasswordEncoder passwordEncoder ;
 	
 	// Get all Users
-	public List<User> getUsers() {
-		return userRepository.findAll();
+	public Page<User> getUsers(int page, int size, String sort, String sortOrder) {
+		Sort.Direction direction = sortOrder.equalsIgnoreCase("ASC") ? Sort.Direction.ASC : Sort.Direction.DESC;
+		Sort sortBy = Sort.by(direction,sort);
+		Pageable pageable = PageRequest.of(page, size, sortBy);
+		return userRepository.findAll(pageable);
 	}
 	
 	// Get one User by UserId
@@ -31,21 +39,35 @@ public class UserService {
 	}
 	
 	// Add User
-	public User addUser(User User) {
-		return userRepository.save(User);
+	public User addUser(User user) {
+		if(userRepository.existsByEmail(user.getEmail()))
+			throw new BadRequestException("User with this email address already exists.");
+		passwordEncoder = new BCryptPasswordEncoder();
+		user.setPassword(passwordEncoder.encode(user.getPassword()));
+		return userRepository.save(user);
 	}
 	
 	// Update User
-	public User updateUser(User User) {
-		if(userRepository.existsById(User.getUserId()))
-			return userRepository.save(User);
+	public User updateUser(User user) {
+		if(user.getUserId() <=0 )
+			throw new BadRequestException("UserId cannot be null or empty.");
+		passwordEncoder = new BCryptPasswordEncoder();
+		user.setPassword(passwordEncoder.encode(user.getPassword()));
+		if(userRepository.existsById(user.getUserId()))
+			return userRepository.save(user);
 		else 
-			return null;
+			throw new NotFoundException("User does not exist with provided userId.");
+
 	}
 	
 	// Delete User
-	public void deleteUser(int UserId) {
-		userRepository.deleteById(UserId);
+	public void deleteUser(int userId) {
+		if(userId <=0 )
+			throw new BadRequestException("adminId id cannot be null or empty.");
+		if(userRepository.existsById(userId))
+			userRepository.deleteById(userId);
+		else 
+			throw new NotFoundException("The admin user does not exist with provided adminId.");
 	}
 	
 	public User validateLogin(LoginRequestDto loginDto) {
